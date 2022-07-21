c1c7151387b113029562517ddc003fa2722d20a1
Guillermo San Martín <guille.seba0802@gmail.com>
2022-07-21 00:13:59 -0300

    Rendezirando tareas desde localStorage

--- script.js
+++ script.js
@@ -4,19 +4,38 @@ const tasksList = document.querySelector(".list");
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   let newTask = document.querySelector("#text-input").value;
+  let myObjOfTasks = { task: newTask };
+  addTask(newTask);
+  myArrayOfTasks(myObjOfTasks);
+});
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  let tasksArr = JSON.parse(localStorage.getItem("task"));
+  tasksArr.forEach((task) => {
+    addTask(Object.values(task));
+  });
+});
+
+function addTask(newTask) {
   const listItem = document.createElement("li");
   tasksList.appendChild(listItem);
   listItem.textContent = newTask;
   listItem.classList.add("list-item");
-
-  const divDelTexto = document.createElement("div");
-  const botonBorrar = document.createElement("button");
-  botonBorrar.innerHTML = "Eliminar";
-  botonBorrar.classList.add("delete-btn");
-  botonBorrar.addEventListener("click", (event) => {
+  form.reset();
+  const deleteBtn = document.createElement("button");
+  listItem.appendChild(deleteBtn);
+  deleteBtn.innerHTML = "Eliminar";
+  deleteBtn.classList.add("delete-btn", "on-focusBtn");
+  deleteBtn.addEventListener("click", (event) => {
     event.target.parentNode.remove();
   });
-  listItem.appendChild(divDelTexto);
-  listItem.appendChild(botonBorrar);
-  form.reset();
-});
+}
+
+let myArray = JSON.parse(localStorage.getItem("task")) || [];
+
+function myArrayOfTasks(myObjOfTasks) {
+  myArray.push(myObjOfTasks);
+  let myObjJSON = JSON.stringify(myArray);
+  localStorage.setItem("task", myObjJSON);
+  console.log(localStorage.getItem("task"));
+}
